9bd1a06b6d35a145f56d5beb3f15142a88313305
Harshita222 <kumariharsita501@gmail.com>
2024-06-27 11:55:34 +0530

    do

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,35 +1,33 @@
-// import React from "react";
-
-// const WeatherDisplay = ({ weatherData }) => {
-//   const { temperature, conditions } = weatherData; // Destructure temperature and conditions from props
-
-//   const tempColor = temperature > 20 ? "red" : "blue";
-//   return (
-//     <>
-//       <div>
-//         <p>
-//           <span style={{ color: tempColor }}>Temperature: {temperature}</span>
-//           <span style={{ color: tempColor }}>Conditions: {conditions}</span>
-//         </p>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default WeatherDisplay;
 import React from "react";
-const WeatherDisplay = ({ weather }) => {
-  const { temperature, conditions } = weather;
-  const tempStyle = {
-    color: temperature > 20 ? "red" : "blue",
-  };
+
+const WeatherDisplay = ({ weatherData }) => {
+  const { temperature, conditions } = weatherData; // Destructure temperature and conditions from props
+
+  const tempColor = temperature > 20 ? "red" : "blue";
   return (
     <div>
       <p>
-        <span style={tempStyle}>Temperature: {temperature}</span>
+        <span style={{ color: tempColor }}>Temperature: {temperature}</span>
       </p>
-      <p>Conditions: {conditions}</p>
+      <p> Conditions: {conditions}</p>
     </div>
   );
 };
-export default WeatherDisplay;
+
+// export default WeatherDisplay;
+// import React from "react";
+// const WeatherDisplay = ({ weather }) => {
+//   const { temperature, conditions } = weather;
+//   const tempStyle = {
+//     color: temperature > 20 ? "red" : "blue",
+//   };
+//   return (
+//     <div>
+//       <p>
+//         <span style={tempStyle}>Temperature: {temperature}</span>
+//       </p>
+//       <p>Conditions: {conditions}</p>
+//     </div>
+//   );
+// };
+// export default WeatherDisplay;
